a643e3b2f9c5b0fcf145488fadbb9f2dfd470af2
root <Manucho09>
2023-07-19 12:14:14 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * abs - computes the absolute value of a no.
+ * @n: no whoose absolute value is to be determined
+ * Return: Absolute valiue of n
+ */
+
+
+
+int abs(int n)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return(-n);
+	}
+
+}
