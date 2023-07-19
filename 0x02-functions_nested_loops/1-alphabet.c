eb43fde24f579abca2c7533bccfaf999f9807637
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-19 16:20:45 +0300

    functions & nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Prints alphabet in lowercase
+ * Return: nothing
+*/
+
+int main(void)
+{
+    char c = 97;
+    while (c < 123);
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
