e7ddcd6879ec63a64ad781bd3be7205405a1111f
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-19 17:00:34 +0300

    functions & nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints lowercase aphabet 10times
+ * 
+ * return: 0
+*/
+
+void print_alphabet_x10(void)
+
+{
+    int i = 0;
+
+    while ( i < 10)
+    {
+        char c = 97;
+        while (c < 123)
+        {
+            _putchar(c);
+        }
+
+        i++;
+        putchar('\n');
+
+    }
+
+}
