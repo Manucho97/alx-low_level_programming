845096b24af99196e852a52f468b5cc6d2e78f6d
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-19 17:35:44 +0300

    functions & nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,6 +14,7 @@ void print_alphabet_x10(void)
     while ( i < 10)
     {
         char c = 97;
+        
         while (c < 123)
         {
             _putchar(c);
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks if c is a lowercase character
+ * Return: 1 if its lower else 0
+ * 
+*/
+
+int _islower(c)
+{
+    if (islower(c))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
