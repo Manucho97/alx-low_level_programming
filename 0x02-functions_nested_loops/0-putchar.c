177b570183a306f304ab8ddffcdf6e42a5283550
root <Manucho09>
2023-07-19 15:04:54 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Print "_putchar" followed by a new line
  * Return: 0
@@ -8,24 +9,24 @@
 
 int main(void)
 {
-    char a = '_';
-    char b = 'p';
-    char c = 'u';
-    char d = 't';
-    char e = 'c';
-    char f = 'h';
-    char g = 'a';
-    char h = 'r';
+	char a = '_';
+	char b = 'p';
+       	char c = 'u';
+	char d = 't';
+       	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
 
-    _putchar(a);
-    _putchar(b);
-    _putchar(c);
-    _putchar(d);
-    _putchar(e);
-    _putchar(f);
-    _putchar(g);
-    _putchar(h);
-    _putchar('\n');
+       	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+       	_putchar(e);
+       	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar('\n');
 
     return 0;
 }
