c0dc5c17a1d77c7c0c2b0bd47f9ca49d1fb32153
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-22 02:37:12 +0300

    functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,11 @@
 void more_numbers(void)
 
 {
-	for (int i = 0; i < 10; i++)
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			{
