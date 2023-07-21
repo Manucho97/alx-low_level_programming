03bb2f8b32e1984dc3afe9466a329dd07c5697f8
root <Manucho09>
2023-07-21 13:51:09 -0700

    functions, nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: 0
+ */
+
+
+void print_numbers(void)
+
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+}
+
