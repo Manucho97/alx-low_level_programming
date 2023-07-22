55cc55f66da740735bd80a1d0b659cf552084e31
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-22 03:51:20 +0300

    functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,12 @@ int main(void)
 
 	for (i = 10; i <= 100; i++)
 	{
-		if (i % 3 = 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 			printf(" ");
