e9b75deadbc0784bf5b3ae1e408000169860bd76
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-22 12:58:34 +0300

    fxns

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * _isupper - checks if a character is uppercase
- * @c - the character to be cheked
- * Return: 1 if the character is
-=======
- * _isupper - checks if a character
- *  is uppercase
- * @c: the character to be cheked
- * Return: 1 if the character is 
->>>>>>> 45c6d55e4599a29bace8541858f54e75a1cc8335
- * uppercase otherwise 0
- */
+ * _isupper - checks if its an uppercase character
+ * @c: character to be checked
+ * Return: 0
+*/
 
 
 int _isupper(int c)
@@ -20,11 +12,11 @@ int _isupper(int c)
 {
 	if (c >= 65 && c < 91)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 
 }
