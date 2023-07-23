b7a99c77a94066c9632750f00c8392b3913f8d9e
root <Manucho09>
2023-07-23 14:02:37 -0700

    prit_numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * Return: 0 always
+ */
+
+int main(void)
+
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
