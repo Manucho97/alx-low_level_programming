1e8c94e2da61c8e7c51c8f528c068255c04743e2
root <Manucho09>
2023-07-23 14:09:00 -0700

    tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase aphabets in reverse
+ * Return: 0 always
+ */
+
+
+int main(void)
+
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+
+	return(0);
+
+}
