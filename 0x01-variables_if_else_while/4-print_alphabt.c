99fe4b18c44cd70f3d128909781cd27148d31436
root <Manucho09>
2023-07-23 13:42:18 -0700

    nest

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphbts excluding e&q
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+
+	char c = 97;
+
+	while (c < 123)
+	{
+		if (c != 101 && c != 113)
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
