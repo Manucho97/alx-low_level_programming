063d0fb3023f7436a8c758c42b310264384bf946
root <Manucho09>
2023-07-23 11:55:13 -0700

    print_alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase followed by new_line
+ * Return: 0 always
+ */
+
+
+int main(void)
+
+{
+	int i;
+
+	i = 65;
+
+	while (i < 91);
+	{
+		putchar(i);
+	}
+	return (0);
+
+}
