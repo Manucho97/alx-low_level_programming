2765bc954e3acc54e05f5afbef46f6135a74ba9d
root <Manucho09>
2023-07-23 14:28:58 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all lwoercase xchers of base 16
+ * Return; 0 always
+ */
+
+
+int main(void)
+
+{
+	int i = 48;
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
