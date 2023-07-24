21832c45356f5cda0907ff1624f044273a6aae78
root <Manucho09>
2023-07-24 15:15:49 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print-rev - prints a string in reverse\n
+ * @s: string to be printed in reverse
+ * Return: nothing
+ */
+
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s); i = 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
+
