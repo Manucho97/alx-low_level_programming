157f4d6bdc6d1aa6e395c18af08a8e149e7833ac
root <Manucho09>
2023-07-24 13:13:47 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @c - the string whoose length is is to be determined
+ * Return: the string length
+ */
+
+
+int _strlen(char *s);
+
+{
+	return (_strlen(*s));
+
+}
