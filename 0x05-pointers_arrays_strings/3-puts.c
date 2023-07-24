3c6a0c9b7b34122cce994763fca9a091c6a8ddeb
root <Manucho09>
2023-07-24 14:34:23 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a  string followed by a new line
+ * @str: pointer to string to be printed
+ * Return: nothing
+ */
+
+void _puts(char *str)
+
+{
+	int i = str[0];
+
+	while ( i != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+}
