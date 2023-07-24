468f1511ca26705118cb3208f8d9c2b6c3fa25b4
root <Manucho09>
2023-07-24 15:27:26 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,17 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
+	int j;
 
-	i = _strlen(s);
-	
-	while (i = 0)
+	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
-		i--;
+		i++;
+	}
+
+	for (j = i; j = 0; j--)
+	{
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 
