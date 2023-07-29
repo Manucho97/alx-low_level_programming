1b2898193d26c002c369ba81a5bc8ad08f437ec3
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-30 00:48:16 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - prints evry other character of a string
+ * @str: the string
+ * Return: nothing
+ */
+
+
+void puts2(char *str)
+
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+
+}
+
