80e001fab2e304c5fb3874965e06d1e9a831bcbe
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-30 00:38:45 +0300

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	}
 	if (length % 2 != 0)
 	{
-		half = (length - 1)  / 2;
+		half = ((length - 1)  / 2) + 1;
 	}
 	else
 	{
@@ -30,6 +30,7 @@ void puts_half(char *str)
 		_putchar(str[half]);
 		half++;
 	}
+	_putchar('\n');
 
 }
 
