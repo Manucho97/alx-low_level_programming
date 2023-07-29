29614b13012bcb4842425769dc188f166902f2e6
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-07-30 00:56:21 +0300

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,16 +14,8 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i % 2 != 0)
-		{
-			_putchar("");
-
-		}
-		else
-		{
-			_putchar(str[i]);
-			i++;
-		}
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 
