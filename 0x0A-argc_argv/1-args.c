f755e81da6ddfd967ab84a8faf69c310599b559d
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-11-02 02:13:17 +0300

    commandline_arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,9 @@
 */
 
 
-int main(int argc, char  __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-    printf("%d\n", argc - 1);
-    
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
