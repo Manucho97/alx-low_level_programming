c9cd6a2e1cfeffa94bd62801a524b19cafb0083b
manucho97 <emmanuelmukaisimasai@gmail.com>
2023-11-02 01:56:04 +0300

    commandline_arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the function name
+ * @argc: number of arguments passed to the function
+ * @argv: An array of pointers to the arguments
+ * 
+ * Return: 0 always
+*/
+
+
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return(0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv: An array of pointers to the arguments
+ * 
+ * Return: 0 always
+*/
+
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for(i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+        return(0);
+    }
+}
